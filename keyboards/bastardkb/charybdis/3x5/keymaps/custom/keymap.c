beb99ae658c43a2aae0dfe5455b8998664fdc941
kainoa-h <kainoahowj@gmail.com>
2026-01-07 20:16:27 +0800

    feat(game): added a gaming layer

--- keyboards/bastardkb/charybdis/3x5/keymaps/custom/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/custom/keymap.c
@@ -26,6 +26,7 @@ enum charybdis_keymap_layers {
     LAYER_SYMB,
     LAYER_SYMB_SHIFT,
     LAYER_FNC,
+    LAYER_GAME,
     LAYER_POINTER,
 };
 
@@ -186,7 +187,7 @@ combo_t key_combos[COMBO_LENGTH] = {
     XXXXXXX, KC_QUOT, KC_LBRC, KC_RBRC, KC_PIPE,          KC_BSLS, KC_7, KC_8, KC_9, KC_PLUS, \
     KC_TAB,  XXXXXXX, KC_LCBR, KC_RCBR, XXXXXXX,          KC_EQL,  KC_4, KC_5, KC_6, KC_0,    \
     KC_GRV,  XXXXXXX, MT(MOD_LALT, KC_LPRN), MT(MOD_LGUI, KC_RPRN), KC_PIPE, KC_UNDS, KC_1, KC_2, KC_3, KC_MINS, \
-             XXXXXXX, KC_BSPC, SFT_BSP,               SFT_BSP, _______
+             TO(LAYER_GAME), KC_BSPC, SFT_BSP,               SFT_BSP, _______
 
 // LAYER_SYMB_SHIFT (ZMK layer_3)
 #define LAYOUT_LAYER_SYMB_SHIFT \
@@ -201,6 +202,13 @@ combo_t key_combos[COMBO_LENGTH] = {
     KC_TAB,  KC_F4, KC_F5, KC_F6, KC_F11,          KC_EQL,  KC_DLR,  KC_PERC, KC_CIRC, KC_QUOT, \
     XXXXXXX, KC_F1, KC_F2, KC_F3, KC_F12,          KC_BSLS, KC_EXLM, KC_AT,   KC_HASH, KC_MINS, \
              XXXXXXX, _______, SFT_LRET,           SFT_LRET, LSFT(KC_ENT)
+
+// LAYER_GAME
+#define LAYOUT_LAYER_GAME \
+     KC_T,    KC_Q,   KC_W,   KC_E,   KC_R,       KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    \
+     KC_LSFT, KC_A,   KC_S,   KC_D,   KC_F,       KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, \
+     KC_LCTL, KC_Z,   CTL_X,  ALT_C,  GUI_V,      KC_N,   GUI_M,  ALT_CM,  CTL_DT,  KC_SLSH, \
+                         KC_B, KC_SPC, KC_G,      TO(LAYER_BASE), RMT_SYMB
 
 // LAYER_POINTER (Existing)
 #define LAYOUT_LAYER_POINTER_CUSTOM \
@@ -221,6 +229,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_SYMB] = LAYOUT_wrapper(LAYOUT_LAYER_SYMB),
   [LAYER_SYMB_SHIFT] = LAYOUT_wrapper(LAYOUT_LAYER_SYMB_SHIFT),
   [LAYER_FNC] = LAYOUT_wrapper(LAYOUT_LAYER_FNC),
+  [LAYER_GAME] = LAYOUT_wrapper(LAYOUT_LAYER_GAME),
   [LAYER_POINTER] = LAYOUT_wrapper(LAYOUT_LAYER_POINTER_CUSTOM),
 };
 
